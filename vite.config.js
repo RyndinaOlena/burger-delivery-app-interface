e96eb1fd17eb85594b2df63a2811cf2e1d891f7e
Ryndina Olena <110286532+RyndinaOlena@users.noreply.github.com>
2025-05-26 13:42:21 +0300

    fix mistakes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://ryndinaolena.github.io/burger-delivery-app-interface/'
+  base: '/burger-delivery-app-interface/',
 })
